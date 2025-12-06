da1bba4f88ed87cb62f8940f9cdb3cee0ccea243
ratislavka <65152341+ratislavka@users.noreply.github.com>
2025-12-06 14:29:03 +0500

    Storyboard References deleted
    Working programatic UI

---
+++ What2Cook/What2Cook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  What2Cook
+//
+//  Created by Ratislav Ovchinnikov on 06.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
